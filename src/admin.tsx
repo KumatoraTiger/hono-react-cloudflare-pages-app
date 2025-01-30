a47cd529827f297841e7415df278171b583ec601
KumatoraTiger <03agiat.eba@gmail.com>
2025-01-30 21:34:09 +0900

    署名付きアップロード

--- src/admin.tsx
+++ src/admin.tsx
@@ -1,12 +1,81 @@
-import { SessionProvider } from '@hono/auth-js/react'
-import { createRoot } from 'react-dom/client'
-import { Header } from './components/header'
+import { SessionProvider } from '@hono/auth-js/react';
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Header } from './components/header';
 
 function App() {
+  const [files, setFiles] = useState<FileList | null>(null);
+
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFiles(event.target.files);
+  }
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!files || files.length === 0) {
+      console.error('No files selected');
+      return;
+    }
+
+    try {
+      const uploadPromises = Array.from(files).map(async (file) => {
+        try {
+          const presignedUrlResponse = await fetch('/api/admin/get-presigned-url');
+
+          if (!presignedUrlResponse.ok) {
+            throw new Error('Failed to fetch presigned URL');
+          }
+
+          const { url, key }: { url: string; key: string } = await presignedUrlResponse.json();
+
+          const uploadResponse: Response = await fetch(url, {
+            method: 'PUT',
+            body: file,
+            headers: {
+              'Content-Type': file.type,
+            },
+          });
+
+          if (uploadResponse.ok) {
+            console.log(`File ${file.name} uploaded successfully!`);
+
+            const completeResponse: Response = await fetch('/upload-complete', {
+              method: 'POST',
+              body: JSON.stringify({ key: key }),
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+
+            if (completeResponse.ok) {
+              console.log('Upload completion notified.');
+            } else {
+              console.error('Failed to notify upload completion.');
+            }
+          } else {
+            throw new Error(`Failed to upload file ${file.name}`);
+          }
+        } catch (error) {
+          console.error(`Error uploading file ${file.name}:`, error);
+        }
+      });
+
+      // すべてのアップロードが完了するのを待つ
+      await Promise.all(uploadPromises);
+      console.log('All files uploaded successfully');
+    } catch (error) {
+      console.error('Error in handleSubmit:', error);
+    }
+  };
+
   return (
     <SessionProvider>
       <Header />
       <h1>Welcome Admin!</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="file" multiple accept="image/*" onChange={handleFileChange}  />
+        <button type="submit">Upload</button>
+      </form>
     </SessionProvider>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,37 @@
 import Google from '@auth/core/providers/google';
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { authHandler, initAuthConfig, verifyAuth } from '@hono/auth-js';
 import { drizzle } from "drizzle-orm/d1";
-import { Hono } from 'hono';
+import { type Context, Hono } from 'hono';
 import { renderToString } from 'react-dom/server';
 
 type Bindings = {
+  AUTH_SECRET: string;
+  GOOGLE_ID: string;
+  GOOGLE_SECRET: string;
+  CLOUDFLARE_REGION: string;
+  CLOUDFLARE_R2_ENDPOINT: string;
+  CLOUDFLARE_R2_BUCKET_NAME: string;
+  CLOUDFLARE_R2_ACCESS_KEY_ID: string;
+  CLOUDFLARE_R2_SECRET_ACCESS_KEY: string;
   DB: D1Database;
-};
+  BUCKET: R2Bucket;
+}
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+const r2Client = (c: Context<{ Bindings: Bindings}>) => {
+  return new S3Client({
+    region: c.env.CLOUDFLARE_REGION,
+    endpoint: c.env.CLOUDFLARE_R2_ENDPOINT,
+    credentials: {
+      accessKeyId: c.env.CLOUDFLARE_R2_ACCESS_KEY_ID,
+      secretAccessKey: c.env.CLOUDFLARE_R2_SECRET_ACCESS_KEY,
+    },
+  });
+}
 
 app.use(
   '*',
@@ -31,6 +53,39 @@ app.use(
 app.use('/api/auth/*', authHandler())
 
 app.use('/admin/*', verifyAuth())
+app.use('/api/admin/*', verifyAuth())
+
+// 署名付き URL 生成エンドポイント
+app.get('/api/admin/get-presigned-url', async (c) => {
+  const client = r2Client(c);
+  const key = crypto.randomUUID();
+  const command = new PutObjectCommand({
+    Bucket: c.env.CLOUDFLARE_R2_BUCKET_NAME,
+    Key: key,
+    ContentType: 'application/octet-stream',
+  });
+
+  const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
+
+  return c.json({ url: signedUrl, key: key });
+});
+
+app.post('/api/admin/upload-complete', async (c) => {
+  const { key } = await c.req.json<{ key: string }>();
+
+  return c.json({ key });
+});
+
+app.get('/objects', async (c) => {
+  const objects = await c.env.BUCKET.list();
+  const client = r2Client(c);
+  const command = new GetObjectCommand({
+    Bucket: c.env.CLOUDFLARE_R2_BUCKET_NAME,
+    Key: objects.objects[0].key,
+  });
+  const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
+  return c.json(signedUrl);
+})
 
 app.get('/', (c) => {
   return c.html(
